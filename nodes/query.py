7b2b611e635fa8bd6425cb05a4be47a46016879e
Bob Paauwe <bpaauwe@yahoo.com>
2021-07-20 15:31:49 -0700

    force send precipitation even if zero

--- nodes/query.py
+++ nodes/query.py
@@ -360,6 +360,10 @@ class queries(object):
                     if v == None or v == "None":
                         v = "0"
                     n.setDriver('PRECIP', round(float(v), prec), True, force, wmap.uom('PRECIP'))
+                else:
+                    n.setDriver('PRECIP', 0, True, force, wmap.uom('PRECIP'))
+            else:
+                n.setDriver('PRECIP', 0, True, force, wmap.uom('PRECIP'))
                 
         except Exception as e:
             LOGGER.error('Precipitation summary update failure: {}'.format(e))
